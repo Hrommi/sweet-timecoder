1f36dd86eb1ed68378df2c6563de1d6dac2a298a
Stepan <goodhrommi@gmail.com>
2019-09-22 18:57:22 +0300

    added control button for VideoPlayer

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Player } from "video-react";
+import { Player, ControlBar, ReplayControl, ForwardControl } from "video-react";
 
 import "../../../node_modules/video-react/dist/video-react.css";
 
@@ -7,6 +7,10 @@ const VideoPlayer = ({ url, innerRef }) => {
   return (
     <Player ref={innerRef}>
       <source src={url} />
+      <ControlBar autoHide={false}>
+        <ReplayControl seconds={5} order={2.1} />
+        <ForwardControl seconds={5} order={3.1} />
+      </ControlBar>
     </Player>
   );
 };
