efb5f6d10351c49d626ae713dc5de47d476e1612
Stepan <goodhrommi@gmail.com>
2019-09-22 16:40:10 +0300

    created base layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      Sweet Timecoder
+      <div className="app__select-file">
+        <input type="file" />
+      </div>
+      <div className="app__container">
+        <div className="app__video">Video</div>
+        <div className="app__actions">Actions</div>
+        <div className="app_timecodes">Timecoded</div>
+      </div>
     </div>
   );
 }
