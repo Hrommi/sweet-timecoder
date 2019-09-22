8e64f269e22df7615a022a9b9952bf30fa9c521b
Stepan <goodhrommi@gmail.com>
2019-09-22 19:50:56 +0300

    added useKeyPress for toggleRecord

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -2,10 +2,23 @@ import React from "react";
 
 import RecordIcon from "../RecordIcon";
 
+import { useKeyPress } from "../../hooks/useKeyPress";
+
 const Actions = ({ toggleRecord, isRecording }) => {
+  const handleRecord = () => {
+    toggleRecord();
+  };
+
+  const recordPress = useKeyPress("Enter");
+  React.useEffect(() => {
+    if (recordPress) {
+      toggleRecord();
+    }
+  }, [recordPress]);
+
   return (
     <div>
-      <button type="button" onClick={toggleRecord}>
+      <button type="button" onClick={handleRecord}>
         Record <RecordIcon rec={isRecording} />
       </button>
     </div>
