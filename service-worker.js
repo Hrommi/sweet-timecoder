f9c1ce8f1d10b8608e06c271beaed12a083c9ecc
Stepan <goodhrommi@gmail.com>
2019-09-22 18:44:04 +0300

    Revert "Updates"
    This reverts commit 2ca933faf2030a5d4eaae9331c0891182c74e41e.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sweet-timecoder/precache-manifest.ec7224ca84bc22e2abf0aa1710cfbc3a.js"
+  "/precache-manifest.521550866e5769dd6d7a4136681123aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sweet-timecoder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
